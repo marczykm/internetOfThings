f8790e9e6e66a423575abd0987c287636595b2c0
Marcin Marczyk <m.l.marczyk@gmail.com>
2015-10-24 18:20:56 +0200

    initial commit

---
+++ src/main/java/pl/marczyk/InternetOfThingsApplication.java
@@ -0,0 +1,12 @@
+package pl.marczyk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternetOfThingsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InternetOfThingsApplication.class, args);
+    }
+}
